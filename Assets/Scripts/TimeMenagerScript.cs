5d3a8beb330af429a237527135c29fe5900c439b
Nina Golenko <nina.golenko@gmail.com>
2024-09-05 11:47:03 +0200

    added timer

--- Assets/Scripts/TimeMenagerScript.cs
+++ Assets/Scripts/TimeMenagerScript.cs
@@ -7,9 +7,25 @@ using TMPro;
 public class TimeMenagerScript : MonoBehaviour
 {
     [SerializeField] TMP_Text timeDisplay;
+    [SerializeField] GameObject pauseScreen;
+    public float inGameTime;
+
+    public void KeepTime()
+    {
+        Time.timeScale = 1.0f;
+        inGameTime += Time.deltaTime * 5f;
+    }
 
     public void Update()
     {
-        
+        if(pauseScreen.gameObject.activeInHierarchy == false)
+        {
+            KeepTime();
+        }
+
+        int minutes = Mathf.FloorToInt(inGameTime / 60f);
+        int seconds = Mathf.FloorToInt(inGameTime - minutes * 60f);
+
+        timeDisplay.text = string.Format("{0:0}:{1:00}", minutes, seconds) + " S.S.T.";
     }
 }
