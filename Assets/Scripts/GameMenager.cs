ef56ca261fd9d4e47184f46f290989c9befeb0a9
Nina Golenko <nina.golenko@gmail.com>
2024-09-05 13:49:04 +0200

    oxygen system WIP

--- Assets/Scripts/GameMenager.cs
+++ Assets/Scripts/GameMenager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameMenager : MonoBehaviour
 {
-    public bool electrolysis;
+    [SerializeField] bool drainOxygen;
+    [SerializeField] TMP_Text O2Display;
     public float oxygen;
 
     private void Start()
     {
         oxygen = 100f;
+        drainOxygen = true;
+    }
+
+    public void Update()
+    {
+        if (drainOxygen == false)
+        {
+            oxygen -= Time.deltaTime;
+        }
+
+        O2Display.text = $"Oxygen Saturation: {oxygen}%";
     }
 }
