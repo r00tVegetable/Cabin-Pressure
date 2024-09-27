a132bfe733dfee8b9f15a39e42d61d1c1ad6fded
r00tVegetable <nina.golenko@gmail.com>
2024-09-27 11:36:48 +0200

    oxygen drain update

--- Assets/Scripts/GameMenager.cs
+++ Assets/Scripts/GameMenager.cs
@@ -9,25 +9,47 @@ public class GameMenager : MonoBehaviour
     [SerializeField] GameObject theStatics;
     [SerializeField] GameObject pauseScreen;
 
-    [SerializeField] bool drainOxygen;
     [SerializeField] TMP_Text O2Display;
+    [SerializeField] float drainSpeed;
+    [SerializeField] bool drainOxygen;  //Change to public when making electrolysis system.
     public float oxygen;
 
     private void Start()
     {
         oxygen = 100f;
         drainOxygen = false;
+        
     }
 
     public void Update()
     {
+        //Oxygen system:
         O2Display.text = $"Oxygen Saturation: {oxygen:0.00}%";
-
+        
         if (drainOxygen == true && pauseScreen.activeInHierarchy == false)
         {
-            oxygen -= Time.deltaTime;
+            oxygen -= Time.deltaTime/drainSpeed;
         }
-
+        if(oxygen >75)
+        {
+            drainSpeed = 40f;
+        }
+        if(oxygen <= 75)
+        {
+            drainSpeed = 30f;
+        }
+        if (oxygen <= 50)
+        {
+            drainSpeed = 20f;
+        }
+        if (oxygen <= 25)
+        {
+            drainSpeed = 10f;
+        }
+        if(oxygen <= 10)
+        {
+            drainSpeed = 5f;
+        }
         if (oxygen <= 0)
         {
             KillMe();
