4d2ca3bd10b09d2bed70dc61aa2cb104b33aced3
Nina Golenko <nina.golenko@gmail.com>
2024-10-14 11:02:50 +0200

    Update DialogueParser

--- Assets/Scripts/DialogueParser.cs
+++ Assets/Scripts/DialogueParser.cs
@@ -46,6 +46,79 @@ public class DialogueParser : MonoBehaviour
     {
         string line;
         StreamReader r = new StreamReader(filename);
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    string[] lineData = line.Split(';');
+                    if (lineData[0] == "Player")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], "", 0, "");
+                        lineEntry.options = new string[lineData.Length - 1];
+                        for (int i = 1; i < lineData.Length; i++)
+                        {
+                            lineEntry.options[i - 1] = lineData[i];
+                        }
+                        lines.Add(lineEntry);
+                    }
+                    else
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], int.Parse(lineData[2]), lineData[3]);
+                        lines.Add(lineEntry);
+                    }
+                }
+            }
+            while (line != null);
+            r.Close();
+        }
     }
 
+    public string GetPosition(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].position;
+        }
+        return "";
+    }
+
+    public string GetName(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].name;
+        }
+        return "";
+    }
+
+    public string GetContent(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].content;
+        }
+        return "";
+    }
+
+    public int GetPose(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].pose;
+        }
+        return 0;
+    }
+
+    public string[] GetOptions(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].options;
+        }
+        return new string[0];
+    }
 }
