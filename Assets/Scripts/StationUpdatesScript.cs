e85ded45fa1b1b82fa217b5d87a84ed7fccf468a
Nina Golenko <nina.golenko@gmail.com>
2024-10-18 11:49:27 +0200

    n

---
+++ Assets/Scripts/StationUpdatesScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationUpdatesScript : MonoBehaviour
+{
+    [SerializeField] HealthScript HealthScript;
+    public void ForwardTimers(float time)
+    {
+
+    }
+}
