eec455dac68ef722fa8087868ef52e018b55fe9a
r00tVegetable <nina.golenko@gmail.com>
2024-10-30 16:01:40 +0100

    sound

--- Assets/Scripts/local UI/MainMenuScript.cs
+++ Assets/Scripts/local UI/MainMenuScript.cs
@@ -7,27 +7,47 @@ public class MainMenuScript : MonoBehaviour
 {
     public GameObject SceneLoadingPanel;
 
+    [SerializeField] AudioSource click;
+
     public void Start()
     {
         SceneLoadingPanel.SetActive(false);
     }
+
+    public void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneLoadingPanel.SetActive(false);
+        }
+    }
+
     public void StartNewGame()
     {
-        SceneManager.LoadScene(1);
+        click.Play();
+        StartCoroutine(countToStart());
     }
 
     public void LoadSaved()
     {
+        click.Play();
         SceneLoadingPanel.SetActive(true);
     }
 
     public void CloseSaved()
     {
+        click.Play();
         SceneLoadingPanel.SetActive(false);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator countToStart()
+    {
+        yield return new WaitForSecondsRealtime(3);
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Scripts/old Scripts/dialogue system/Character.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Sprite[] characterPoses = null;
+    public Sprite[] characterFaces = null;
+}
