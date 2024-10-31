f3fe9e7dd84db0dcfb2500b629f301f98ea73678
r00tVegetable <nina.golenko@gmail.com>
2024-10-31 02:01:00 +0100

    v

--- Assets/Scripts/InventroyMenager.cs
+++ Assets/Scripts/InventroyMenager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InventroyMenager : MonoBehaviour
 {
     public int food;
-    public int water;
+    public float water;
     public int alloy;
     public int carbon;
+
+    [SerializeField] TMP_Text foodText;
+    [SerializeField] TMP_Text waterText;
+    [SerializeField] TMP_Text alloyText;
+    [SerializeField] TMP_Text carbonText;
 
     public void Start()
     {
@@ -15,5 +21,13 @@ public class InventroyMenager : MonoBehaviour
         water = 21;
         alloy = 45;
         carbon = 15;
+    }
+
+    public void Update()
+    {
+        foodText.text = $"{food} packs";
+        waterText.text = string.Format("{0:00}", water) + " L";
+        alloyText.text = $"{alloy} kg";
+        carbonText.text = $"{carbon} kg";
     }
 }
